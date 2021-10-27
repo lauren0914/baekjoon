5a2231595460ab9b3ca7f85a80b8fb6d97332207
lauren0914 <cofks05@likelion.org>
2021-10-28 01:46:31 +0900

    백준 문제풀이

---
+++ 1, 2, 3 더하기.py
@@ -0,0 +1,16 @@
+def solve(n):
+    if n == 1:
+        return 1
+    elif n == 2:
+        return 2
+    elif n == 3:
+        return 4
+    else:
+        return solve(n-1) + solve(n-2) + solve(n-3) # solve(6) + solve(5) + solve(4)
+
+T = int(input()) # 3
+
+for _ in range(T):
+    n = int(input())
+    print(solve(n))
+
